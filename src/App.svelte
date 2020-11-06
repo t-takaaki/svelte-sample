8888e1a599b5e305532fd42b490a3e7d5c73f3d3
t-takaaki <hokahoka.oden@gmail.com>
2020-11-06 11:09:05 +0900

    fix indent

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,15 @@
   import Timer from './Timer.svelte';
 
   // Declaring props
-	export let name, hoge, fuga;
+  export let name, hoge, fuga;
   let piyo = "OK!";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <h1>Hello {name}!</h1>
   <h2>{hoge} is {fuga}</h2>
   <h3>{piyo}!</h3>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
   <hr />
   <Counter />
   <hr />
@@ -23,23 +23,23 @@
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
